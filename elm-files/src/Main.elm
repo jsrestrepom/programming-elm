2765256ea20ac3effeda3da9d7b2c81df1585f4b
Javier Santiago Restrepo Moncada <jrestrepo@sura.com.co>
2019-07-22 16:48:26 -0500

    Initial commit

---
+++ elm-files/src/Main.elm
@@ -0,0 +1,42 @@
+module Main exposing (main)
+
+import Html exposing (text)
+
+
+main =
+    text (bottlesOf "juice" 25)
+
+
+greeting : String
+greeting =
+    "Hello, Static Elm!"
+
+
+meaningOfLife : Int
+meaningOfLife =
+    42
+
+
+pi : Float
+pi =
+    3.14
+
+
+canChuck : Bool
+canChuck =
+    True
+
+
+sayHello : String -> String
+sayHello name =
+    "Hello, " ++ name ++ "."
+
+
+bottlesOf : String -> (Int -> String)
+bottlesOf contents amount =
+    String.fromInt amount ++ " bottles of " ++ contents ++ " on the wall."
+
+
+add : Int -> Int -> String
+add x y =
+    String.fromInt (x + y)
