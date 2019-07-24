0a163e116a21850c9be03e6670a7b60c1816bc6a
jsrestrepom <santyrestrepom@gmail.com>
2019-07-23 21:40:51 -0500

    Chapter two. Handle State Changes

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,7 +1,9 @@
 module Picshare exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 
 
 baseUrl : String
@@ -9,36 +11,82 @@ baseUrl =
     "https://programming-elm.com/"
 
 
-initialModel : { url : String, caption : String }
+initialModel : { url : String, caption : String, liked : Bool }
 initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfing"
+    , liked = False
     }
 
 
-viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
 viewDetailedPhoto model =
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+
+            else
+                "fa-heart-o"
+
+        msg =
+            if model.liked then
+                Unlike
+
+            else
+                Like
+    in
     div [ class "detailed-photo" ]
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ h2 [ class "caption" ] [ text model.caption ] ]
+            [ div [ class "like-button" ]
+                [ i
+                    [ class "fa fa-2x"
+                    , class buttonClass
+                    , onClick msg
+                    ]
+                    []
+                ]
+            , h2 [ class "caption" ] [ text model.caption ]
+            ]
         ]
 
 
-view : { url : String, caption : String } -> Html msg
+view : { url : String, caption : String, liked : Bool } -> Html Msg
 view model =
     div []
         [ div [ class "header" ]
             [ h1 [] [ text "Picshare-25" ] ]
         , div [ class "content-flow" ]
             [ viewDetailedPhoto model
-
-            --, viewDetailedPhoto (baseUrl ++ "2.jpg") "The Fox"
-            --, viewDetailedPhoto (baseUrl ++ "3.jpg") "Evening"
+            , viewDetailedPhoto { model | url = baseUrl ++ "2.jpg", caption = "The Fox" }
+            , viewDetailedPhoto { model | url = baseUrl ++ "3.jpg", caption = "Evening" }
             ]
         ]
 
 
-main : Html msg
+type Msg
+    = Like
+    | Unlike
+
+
+update :
+    Msg
+    -> { url : String, caption : String, liked : Bool }
+    -> { url : String, caption : String, liked : Bool }
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+
+        Unlike ->
+            { model | liked = False }
+
+
+main : Program () { url : String, caption : String, liked : Bool } Msg
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
